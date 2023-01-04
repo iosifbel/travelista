dadfb7c5e3563a08c81acaaface8817d1148cd65
David Nitu <davidnitu44@gmail.com>
2023-01-04 18:43:47 +0000

    basic file structure with views and components added + react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LandingPage } from './views/landing/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
